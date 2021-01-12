84f5634dd012c80e62433aa1a956c319cd2ab699
X-San <ytbaconboi95@gmail.com>
2021-01-12 22:35:19 +0000

    starting to add classes

--- cmds/slash.js
+++ cmds/slash.js
@@ -1,6 +1,22 @@
 const Discord=require('discord.js')
 const fetch = require('node-fetch')
 
+class ApplicationCommand {
+	constructor() {
+		this.id = undefined
+		this.application_id = undefined
+		this.name = undefined
+		this.description = undefined
+		this.options = new Discord.Collection()
+		this.subs = new Discord.Collection()
+		this.subgroups = new Discord.Collection()
+	}
+}
+
+class ApplicationCommandOption {
+
+}
+
 function stringFrom(arr, i) {
 	return arr.slice(i).join(' ')
 }
@@ -10,8 +26,7 @@ function CommandUI(message, args) {
 	let sel = "-1"
 	let user = message.author.id
 	let msg
-	let newembed
-	let copts = new Map()
+	let newcmd
 
 	function RootMenu() {
 		let menu = new Discord.MessageEmbed()
@@ -43,19 +58,36 @@ function CommandUI(message, args) {
 			})
 	}
 
+	function CMenuRoot() {
+		let cmenu = new Discord.MessageEmbed()
+		let paramStr = ''
+		newcmd.each((v, i) => {
+			if (i.type in [3,4,5,6,7,8]) {
+				paramStr += '()'
+			}
+		})
+		cmenu.setTitle('Create Command')
+		cmenu.addField('General', `
+		Name*: ${newcmd.name} 
+		Description*: ${newcmd.description}
+		`)
+		cmenu.addField('Paramaters', paramStr)
+		msg.edit(cmenu)
+	}
+
 	channel.send(RootMenu())
 		.then(m => msg = m)
 	
 	function cmd(m) {
 		if (user != m.author.id) return
 		c = m.content.split(' ')
+		if (c[1]) p1 = c[1].toUpperCase()
 		switch (c[0].toUpperCase()) {
 			case 'EXIT':
 				col.stop()
 				channel.send('Exited')
 				break
 			case 'SEL':
-				p1 = c[1].toUpperCase()
 				if (p1 == 'GUILD') {
 					if (c[2]) sel = c[2]
 					else sel = message.guild.id
@@ -67,40 +99,23 @@ function CommandUI(message, args) {
 				break
 			case 'CREATE':
 				if (sel != "-1") {
-					let cmenu = new Discord.MessageEmbed()
-					cmenu.setTitle('Create Command')
-					cmenu.addField('Name*', '(NULL)')
-					cmenu.addField('Desc*', '(NULL)')
-					cmenu.addField('Options','<SET OPTIONS>')
-					msg.edit(cmenu)
-					newcmd = {name: "(NULL)", description: "(NULL)"}
+					newcmd = new UnpreparedCommand()
+					CMenuRoot()
 				}
 				break
 			case 'DELETE':
 				break
 			case 'SET':
-				p1 = c[1].toUpperCase()
 				if (p1 == 'NAME') {
 					newcmd.name = c[2]
-					let cmenu = new Discord.MessageEmbed()
-					cmenu.setTitle('Create Command')
-					cmenu.addField('Name*', `${newcmd.name}`)
-					cmenu.addField('Desc*', `${newcmd.description}`)
-					cmenu.addField('Options','<SET OPTIONS>')
-					msg.edit(cmenu)
+					CMenuRoot()
 				}
 				else if (p1 == 'DESCRIPTION') {
 					newcmd.description = stringFrom(c, 2)
-					let cmenu = new Discord.MessageEmbed()
-					cmenu.setTitle('Create Command')
-					cmenu.addField('Name*', `${newcmd.name}`)
-					cmenu.addField('Desc*', `${newcmd.description}`)
-					cmenu.addField('Options','<SET OPTIONS>')
-					msg.edit(cmenu)
+					CMenuRoot()
 				}
 				break
 			case 'PARAM':
-				p1 = c[1].toUpperCase()
 				if (p1 == 'CREATE') {
 					p2 = c[2].toUpperCase()
 					p3 = c[3]
@@ -123,16 +138,11 @@ function CommandUI(message, args) {
 				}
 				break
 			case 'WRITE':
-				newcmd.options = []
-				for (v of copts) {
-					newcmd.options.push(v[1])
-				}
-
 				console.log(newcmd)
 
 				fetch(`https://discord.com/api/v8/applications/${process.env.clientid}/guilds/${sel}/commands`, {
 					method: "POST",
-					body: JSON.stringify(newcmd),
+					body: JSON.stringify(newcmd.prepare()),
 					headers: {
 						"Content-Type":"application/json",
 						"Authorization": `Bot ${process.env.discordtoken}`
@@ -152,6 +162,6 @@ function CommandUI(message, args) {
 module.exports = {
 	permissions: {group: ['owners']},
 	exec(args,message) {
-		manager = CommandUI(message, args)
+		//manager = CommandUI(message, args)
 	}
 }
