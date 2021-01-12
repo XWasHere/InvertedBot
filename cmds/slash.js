6066e23951b9cdab4eba5ccc34401b5970b1d5d3
X-San <ytbaconboi95@gmail.com>
2021-01-12 21:17:43 +0000

    more changes

--- cmds/slash.js
+++ cmds/slash.js
@@ -16,12 +16,11 @@ function CommandUI(message, args) {
 	function RootMenu() {
 		let menu = new Discord.MessageEmbed()
 		menu.setTitle('Slash Command Editor')
-		menu.addField('COMMANDS', `
+		menu.setDescription(`
 		Global <SEL GLOBAL>
 		Guild  <SEL GUILD>
 		Exit   <EXIT>
 		`)
-		menu.setFooter('ROOT')
 		return menu
 	}
 
@@ -35,128 +34,116 @@ function CommandUI(message, args) {
 			.then(r => {
 				let menu = new Discord.MessageEmbed()
 				menu.setTitle(`Commands for ${id}`)
+				menutext = ""
 				r.forEach(a => {
-					menu.addField(a.id, a.name)
+					menutext += `${a.name} (${a.id})\n`
 				})
-				menu.setFooter('ROOT > ' + id)
+				menu.setDescription(menutext)
 				msg.edit(menu)
-			},console.error)
+			})
 	}
+
 	channel.send(RootMenu())
 		.then(m => msg = m)
 	
 	function cmd(m) {
 		if (user != m.author.id) return
 		c = m.content.split(' ')
-		cmd = c[0].toUpperCase()
-		if (cmd == 'EXIT') {
-			col.stop()
-			channel.send('Exited')
-		}
-		else if (cmd == 'SEL') {
-			p1 = c[1].toUpperCase()
-			if (p1 == 'GUILD') {
-				if (c[2]) {
-					sel = c[2]
+		switch (c[0].toUpperCase()) {
+			case 'EXIT':
+				col.stop()
+				channel.send('Exited')
+				break
+			case 'SEL':
+				p1 = c[1].toUpperCase()
+				if (p1 == 'GUILD') {
+					if (c[2]) sel = c[2]
+					else sel = message.guild.id
+					GuildMenu(sel)
 				}
-				else {
-					sel = message.guild.id
+				else if (p1 == 'GLOBAL') {
+					
 				}
-				GuildMenu(sel)
-			}
-			else if (p1 == 'GLOBAL') {
-				
-			}
-		}
-		else if (cmd == 'CREATE') {
-			if (sel != "-1") {
-				let cmenu = new Discord.MessageEmbed()
-				cmenu.setTitle('Create Command')
-				cmenu.addField('Name*', '(NULL)')
-				cmenu.addField('Desc*', '(NULL)')
-				cmenu.addField('Options','<SET OPTIONS>')
-				msg.edit(cmenu)
-				newcmd = {name: "(NULL)", description: "(NULL)"}
-			}
-		}
-		else if (cmd == 'DELETE') {
-			
-		}
-		else if (cmd == 'SET') {
-			p1 = c[1].toUpperCase()
-			if (p1 == 'NAME') {
-				newcmd.name = c[2]
-				let cmenu = new Discord.MessageEmbed()
-				cmenu.setTitle('Create Command')
-				cmenu.addField('Name*', `${newcmd.name}`)
-				cmenu.addField('Desc*', `${newcmd.description}`)
-				cmenu.addField('Options','<SET OPTIONS>')
-				msg.edit(cmenu)
-			}
-			else if (p1 == 'DESCRIPTION') {
-				newcmd.description = stringFrom(c, 2)
-				let cmenu = new Discord.MessageEmbed()
-				cmenu.setTitle('Create Command')
-				cmenu.addField('Name*', `${newcmd.name}`)
-				cmenu.addField('Desc*', `${newcmd.description}`)
-				cmenu.addField('Options','<SET OPTIONS>')
-				msg.edit(cmenu)
-			}
-		}
-		else if (cmd == 'PARAM') {
-			p1 = c[1].toUpperCase()
-			if (p1 == 'CREATE') {
-				p2 = c[2].toUpperCase()
-				p3 = c[3]
-				if (p2 == 'STRING') {
-					copts.set(p3,{type: 3, name: c[3]})
+				break
+			case 'CREATE':
+				if (sel != "-1") {
+					let cmenu = new Discord.MessageEmbed()
+					cmenu.setTitle('Create Command')
+					cmenu.addField('Name*', '(NULL)')
+					cmenu.addField('Desc*', '(NULL)')
+					cmenu.addField('Options','<SET OPTIONS>')
+					msg.edit(cmenu)
+					newcmd = {name: "(NULL)", description: "(NULL)"}
 				}
-				else if (p2 == 'INT') {
-					copts.set(p3,{type: 4, name: c[3]})
+				break
+			case 'DELETE':
+				break
+			case 'SET':
+				p1 = c[1].toUpperCase()
+				if (p1 == 'NAME') {
+					newcmd.name = c[2]
+					let cmenu = new Discord.MessageEmbed()
+					cmenu.setTitle('Create Command')
+					cmenu.addField('Name*', `${newcmd.name}`)
+					cmenu.addField('Desc*', `${newcmd.description}`)
+					cmenu.addField('Options','<SET OPTIONS>')
+					msg.edit(cmenu)
 				}
-				else if (p2 == 'BOOL') {
-					copts.set(p3,{type: 5, name: c[3]})
+				else if (p1 == 'DESCRIPTION') {
+					newcmd.description = stringFrom(c, 2)
+					let cmenu = new Discord.MessageEmbed()
+					cmenu.setTitle('Create Command')
+					cmenu.addField('Name*', `${newcmd.name}`)
+					cmenu.addField('Desc*', `${newcmd.description}`)
+					cmenu.addField('Options','<SET OPTIONS>')
+					msg.edit(cmenu)
 				}
-			}
-			else if (p1 == 'SET') {
-				p2 = c[2].toUpperCase()
-				if (p2 == 'DESCRIPTION') {
+				break
+			case 'PARAM':
+				p1 = c[1].toUpperCase()
+				if (p1 == 'CREATE') {
+					p2 = c[2].toUpperCase()
 					p3 = c[3]
-					args2 = stringFrom(c, 4)
-					copts.get(p3).description = args2
+					if (p2 == 'STRING') copts.set(p3,{type: 3, name:p3})
+					else if (p2 == 'INT') copts.set(p3,{type: 4, name: p3})
+					else if (p2 == 'BOOL') copts.set(p3,{type: 5, name: p3})
 				}
-				else if (p2 == 'REQUIRED') {
-					p4 = c[4].toUpperCase()
-					if (p4 == 'TRUE') {
-						copts.get(c[3]).required = true
+				else if (p1 == 'SET') {
+					p2 = c[2].toUpperCase()
+					if (p2 == 'DESCRIPTION') {
+						p3 = c[3]
+						args2 = stringFrom(c, 4)
+						copts.get(p3).description = args2
 					}
-					if (p4 == 'FALSE') {
-						copts.get(c[3]).required = false
+					else if (p2 == 'REQUIRED') {
+						p4 = c[4].toUpperCase()
+						if (p4 == 'TRUE') copts.get(c[3]).required = true
+						else if (p4 == 'FALSE') copts.get(c[3]).required = false
 					}
 				}
-			}
-		}
-		else if (cmd == 'WRITE') {
-			newcmd.options = []
-			for (v of copts) {
-				newcmd.options.push(v[1])
-			}
-
-			console.log(newcmd)
-
-			fetch(`https://discord.com/api/v8/applications/${process.env.clientid}/guilds/${sel}/commands`, {
-				method: "POST",
-				body: JSON.stringify(newcmd),
-				headers: {
-					"Content-Type":"application/json",
-					"Authorization": `Bot ${process.env.discordtoken}`
+				break
+			case 'WRITE':
+				newcmd.options = []
+				for (v of copts) {
+					newcmd.options.push(v[1])
 				}
-			})
-				.then(r => {
-					r.json().then(j => {
-						GuildMenu(sel)
-					})
+
+				console.log(newcmd)
+
+				fetch(`https://discord.com/api/v8/applications/${process.env.clientid}/guilds/${sel}/commands`, {
+					method: "POST",
+					body: JSON.stringify(newcmd),
+					headers: {
+						"Content-Type":"application/json",
+						"Authorization": `Bot ${process.env.discordtoken}`
+					}
 				})
+					.then(r => {
+						r.json().then(j => {
+							GuildMenu(sel)
+						})
+					})
+				break
 		}
 	}
 	let col = new Discord.MessageCollector(channel, cmd)
